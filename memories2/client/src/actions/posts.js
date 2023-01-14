68a7f6b121634a9bb48cacb20ca47bd3e69e279e
juda-star <juda.ishta@gmail.com>
2023-01-14 22:41:07 +0200

    initial

--- memories2/client/src/actions/posts.js
+++ memories2/client/src/actions/posts.js
@@ -7,8 +7,8 @@ export const getPosts = (page) => async (dispatch) => {
     dispatch({type:START_LOADING})
     const { data } = await api.fetchPosts(page);
     console.log(data);
-dispatch({type:END_LOADING})
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({type:END_LOADING})
   } catch (error) {
     console.log(error);
   }
